9dd96dbcf26ab22c77ba3d38fad5c177791408b8
ninthcircle <mainlakshayhoon@gmail.com>
2026-02-07 18:32:23 +0530

    Updated build script

--- build.ps1
+++ build.ps1
@@ -1,7 +1,41 @@
+function Log {
+    param (
+        [Parameter(Mandatory)]
+        [string]$Level,
+        [Parameter(Mandatory)]
+        [ConsoleColor]$Color,
+        [Parameter(Mandatory)]
+        [string]$Message
+    )
+    Write-Host "[" -NoNewline
+    Write-Host "$Level" -ForegroundColor $Color -NoNewline
+    Write-Host "] $Message"
+}
+
+function Error {
+    param (
+        [Parameter(Mandatory)]
+        [string]$Message,
+        [switch]$Exit
+    )
+    Log -Level "ERROR" -Color Red -Message $Message
+    if ($Exit) {
+        exit 1
+    }
+}
+
+function Info {
+    param (
+        [Parameter(Mandatory)]
+        [string]$Message
+    )
+    Log -Level "INFO" -Color Blue -Message $Message
+}
+
 New-Item -ItemType Directory -Path "dist" -Force | Out-Null
 
 Get-ChildItem "resumes/resume-*.typ" | ForEach-Object {
     $out = Join-Path "dist" ([System.IO.Path]::GetFileNameWithoutExtension($_.Name) + ".pdf")
     typst compile --root "./" $_.FullName $out
-    Write-Host "[INFO] Generated '$out'."
+    Info -Message "Generated '$out'."
 }
