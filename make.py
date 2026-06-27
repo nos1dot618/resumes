7b3ee2a4d8c80540fb6b766c42b20d1ec6684a45
ninthcircle <mainlakshayhoon@gmail.com>
2026-06-27 14:54:35 +0530

    BUILD: Generate .PNGs out of generated .PDFs

--- make.py
+++ make.py
@@ -1,12 +1,13 @@
 import argparse
 import csv
 from pathlib import Path
-import requests
 import subprocess
 import sys
 
 URL = "https://gitlab.com/api/v4/users/ninthcircle/projects"
-OUTPUT_DIR = "dist"
+OUTPUT_DIR = Path("dist")
+RESUMES_DIR = Path("resumes")
+VENV_DIR = Path("venv")
 
 
 def log(level, message):
@@ -23,29 +24,66 @@ def info(message):
     log("INFO", message)
 
 
-def subcommand_build():
-    resumes_dir = Path("resumes")
-    dist_dir = Path(OUTPUT_DIR)
-    dist_dir.mkdir(exist_ok=True)
+def python():
+    venv_dir = Path(VENV_DIR)
+    if sys.platform == "win32":
+        return str(venv_dir / "Scripts" / "python.exe")
+    else:
+        return str(venv_dir / "bin" / "python")
 
-    for file in resumes_dir.rglob("*resume-*.typ"):
-        rel_parent = file.parent.relative_to(resumes_dir)
-        out_dir = dist_dir / rel_parent
+
+def subcommand_build():
+    OUTPUT_DIR.mkdir(exist_ok=True)
+
+    for file in RESUMES_DIR.rglob("*resume-*.typ"):
+        rel_parent = file.parent.relative_to(RESUMES_DIR)
+        out_dir = OUTPUT_DIR / rel_parent
         out_dir.mkdir(parents=True, exist_ok=True)
-        out = out_dir / f"{file.stem}.pdf"
+        out_pdf = str(out_dir / f"{file.stem}.pdf")
+        out_png = str(out_dir / f"{file.stem}.png")
 
         subprocess.run(
-            ["typst", "compile", "--root", ".", str(file), str(out)], check=True
+            ["typst", "compile", "--root", ".", str(file), out_pdf],
+            check=True,
         )
-        info(f"Generated '{out}'.")
+
+        info(f"Generated '{out_pdf}'.")
+
+        import fitz
+        pdf_document = fitz.open(out_pdf)
+        page = pdf_document[0]
+        pix = page.get_pixmap(dpi=300)
+        pix.save(out_png)
+
+        info(f"Generated '{out_png}'.")
 
 
 def subcommand_setup():
-    subprocess.run(["git", "config", "core.hooksPath", ".githooks"], check=True)
-    info("Git hooks configured.")
+    subprocess.run(
+        ["git", "config", "core.hooksPath", ".githooks"],
+        check=True,
+    )
+    info("Configured Git hooks.")
+
+    subprocess.run(
+        [sys.executable, "-m", "venv", str(VENV_DIR)],
+        check=True,
+    )
+    info("Created virtual environment.")
+
+    subprocess.run(
+        [python(), "-m", "pip", "install", "--upgrade", "pip"],
+        check=True,
+    )
+    subprocess.run(
+        [python(), "-m", "pip", "install", "-r", "requirements.txt"],
+        check=True,
+    )
+    info("Installed development dependencies.")
 
 
 def subcommand_generate_projects_csv():
+    import requests
     out = "dist/gitlab_repos.csv"
     params = {"visibility": "public", "per_page": 200}
 
@@ -71,7 +109,7 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser(prog="make.py", description="Resume build tool")
 
     subparsers = parser.add_subparsers(dest="command", required=True)
-    subparsers.add_parser("setup", help="Configure Git hooks")
+    subparsers.add_parser("setup", help="Configure Git hooks and installs dependencies")
     subparsers.add_parser(
         "build", help="Build all the resumes present inside the resumes/ directory"
     )
